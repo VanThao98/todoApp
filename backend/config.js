73926f8568bab7885d4362b2f09fa7be8b8ef147
vanthao98 <levanthaolvt228@gmail.com>
2024-07-17 14:53:45 +0700

    delete code config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,1 @@
-
-// export const mongoDB_url = 'mongodb+srv://vanthao98:8Y4hYsp%23r75rnfV@book-store-mern.uj71fng.mongodb.net/todo-collection?retryWrites=true&w=majority';
 export const JWT_SECRET = 'areadylongsecretdhfgjhgfdjghfd';
